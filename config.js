3d05c0f573a8c9aa136b762983d61b36e5eba35b
TejadaINC/Repositorios <wmteja.da@gmail.com>
2026-09-12 17:46:57 -0500

    Change CSV_URL to new Google Sheets link
    Updated CSV_URL to point to a new Google Sheets link.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
    ============================================================ */
 const CONFIG = {
   // URL del CSV publicado (Archivo → Compartir → Publicar en la web → CSV)
-  CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-XXXX/pub?gid=0&single=true&output=csv',
+  CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR97GQAnT1XyTGfhB2gFpjCZRIQNjXmlpPui9JkjBH7VuVMaKIp-GHKM1gDu3snMJ_ybFnkLc5MBxl8/pub?gid=0&single=true&output=csv',
 
   // Título
   TITULO_PREFIX: 'Dashboard de',
